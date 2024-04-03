c174d19ace5de8d58856568167355fe65b91841e
aaf40 <aaf40@txstate.edu>
2024-04-02 21:19:34 -0500

    formatted output need to fix calc

--- Main.cpp
+++ Main.cpp
@@ -1,17 +1,23 @@
-#include<iostream>
-#include<string>
-#include<bitset>
+#include <iostream>
+#include <string>
+#include <bitset>
 
-void printFormattedBits(unsigned int bits) {
+void printFormattedBits(unsigned int bits, const std::string &label)
+{
     std::bitset<32> bitset(bits);
     std::string bitString = bitset.to_string();
-    std::cout << bitString.substr(0, 1) << " ";
-    std::cout <<  bitString.substr(1, 8) << " ";
-    std::cout <<  bitString.substr(9, 23) << std::endl;
+    if (!label.empty()) {
+        std::cout << label << ": " << bitString.substr(0, 1) << " " << bitString.substr(1, 8) << " " << bitString.substr(9, 23) << std::endl;
+    } else {
+        std::cout << bitString.substr(0, 1) << " " << bitString.substr(1, 8) << " " << bitString.substr(9, 23) << std::endl;
+    }
 }
 
-int main(int argc, char* argv[]) {
-    if (argc != 3) {
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
         std::cout << "Error: Program requires exactly two floating-point values as input." << std::endl;
         return 1;
     }
@@ -19,35 +25,35 @@ int main(int argc, char* argv[]) {
     float loopBound = std::stof(argv[1]);
     float loopIncrement = std::stof(argv[2]);
 
-    unsigned int* loopBoundBitsPtr = reinterpret_cast<unsigned int*>(&loopBound);
-    std::bitset<32> loopBoundBits(*loopBoundBitsPtr);
+    unsigned int *loopBoundBitsPtr = reinterpret_cast<unsigned int *>(&loopBound);
+    unsigned int *loopIncrementBitsPtr = reinterpret_cast<unsigned int *>(&loopIncrement);
 
-    unsigned int* loopIncrementBitsPtr = reinterpret_cast<unsigned int*>(&loopIncrement);
-    std::bitset<32> loopIncrementBits(*loopIncrementBitsPtr);
-
-    std::cout << "Loop Bound Bits: " << loopBoundBits << std::endl;
-    printFormattedBits(*loopBoundBitsPtr);
-    std::cout << "Loop Increment Bits: " << loopIncrementBits << std::endl;
-    printFormattedBits(*loopIncrementBitsPtr);
+    printFormattedBits(*loopBoundBitsPtr, "Loop Bound");
+    printFormattedBits(*loopIncrementBitsPtr, "Loop Counter");
 
     unsigned int loopBoundExponent = ((*loopBoundBitsPtr >> 23) & 0xFF);
     unsigned int loopIncrementExponent = ((*loopIncrementBitsPtr >> 23) & 0xFF);
 
     int exponentDifference = loopBoundExponent - loopIncrementExponent;
 
-    if (exponentDifference >= 24) {
-        std::cout << "Warning: Possible overflow!" << std::endl;
+    if (exponentDifference >= 24)
+    {
+        std::cout << "\nWarning: Possible overflow!" << std::endl;
+        std::cout << "Overflow threshold:" << std::endl;
 
         unsigned int thresholdExponent = loopBoundExponent - 24;
-        unsigned int thresholdBits = (thresholdExponent << 23); 
-        float* thresholdPtr = reinterpret_cast<float*>(&thresholdBits);
+        unsigned int thresholdBits = (thresholdExponent << 23);
+        float *thresholdPtr = reinterpret_cast<float *>(&thresholdBits);
         float overflowThreshold = *thresholdPtr;
 
-        std::cout << "Overflow threshold: " << overflowThreshold << std::endl;
-        std::bitset<32> thresholdBitset(thresholdBits);
-        std::cout << "Threshold Bits: " << thresholdBitset << std::endl;
-    } else {
-        std::cout << "No overflow detected." << std::endl;
+        std::cout << "        " << overflowThreshold << std::endl;
+         std::cout << "        ";
+
+        printFormattedBits(thresholdBits, "");
+    }
+    else
+    {
+        std::cout << "\nThere is no overflow!" << std::endl;
     }
 
     return 0;
